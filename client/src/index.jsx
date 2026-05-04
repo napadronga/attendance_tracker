1cb9a6b270f185d3411132ed6460b11a1cd6f179
Jesse <jessejamestomlin@gmail.com>
2026-05-03 22:55:42 -0500

    Update index.jsx
    updated index.jsx to include NavBar and logout function

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,18 +1,25 @@
 import { StrictMode, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import NavBar from './components/NavBar.jsx';
 import Login from './pages/Login.jsx'
 import TeacherOverview from './pages/TeacherOverview.jsx'
 import TeacherMark from './pages/TeacherMark.jsx'
 import TeacherHistory from './pages/TeacherHistory.jsx'
 import StudentOverview from './pages/StudentOverview.jsx'
 import StudentAttendance from './pages/StudentAttendance.jsx'
+// still making this import './index.css'
 
 function App() {
   const [user, setUser] = useState(null);
 
+  function logOut(){
+    setUser(null);
+  }
+
   return (
     <BrowserRouter>
+      {user && <NavBar user={user} logout(logOut) />}
       <Routes>
         <Route path="/" element={<Login onLogin={setUser} />} />
         <Route path="/teacher/overview" element={<TeacherOverview user={user} />} />
